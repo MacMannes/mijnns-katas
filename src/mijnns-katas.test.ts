dd69e1b79d1054bc40a20f146a4b9dbd5b12a3f0
André Mathlener <andre.mathlener@ns.nl>
2024-12-06 11:54:43 +0100

    Deleted stub files

--- src/mijnns-katas.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { MijnNSKatas } from './mijnns-katas';
-
-describe('MijnNS Katas', () => {
-    it('Creating an instance of the MijnNSKatas class should work', () => {
-        const clazz = new MijnNSKatas();
-        expect(clazz).toBeDefined();
-    });
-});
