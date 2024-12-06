4d78f7a6e9f0c50b3f68c535876229a8c227b184
André Mathlener <andre.mathlener@ns.nl>
2024-12-06 12:34:36 +0100

    Added pseudo code and acceptance test from previous week

--- src/carPark-escape/carpark.test.ts
+++ src/carPark-escape/carpark.test.ts
@@ -1,14 +1,27 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { CarPark } from './carpark';
 
-describe('noCarScenario', () => {
-    it('Given our car park has no car when we call the escape then we will receive an empty array', () => {
-        const carParkInstance = new CarPark();
-        const carParkNoCars = [
-            [1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-        ];
-        const esacapeRoute = carParkInstance.escape(carParkNoCars);
-        expect(esacapeRoute).toStrictEqual([]);
+describe('CarPark Escape Kata', () => {
+    // describe('CarPark Escape Acceptance', () => {
+    //     test('Escape carpark from the first floor', () => {
+    //         const carParkInstance = new CarPark();
+    //         const carParkAcceptance = [
+    //             [1, 0, 0, 0, 2],
+    //             [0, 0, 0, 0, 0],
+    //         ];
+    //         expect(carParkInstance.escape(carParkAcceptance)).toEqual(['L4', 'D1', 'R4']);
+    //     });
+    // });
+
+    describe('noCarScenario', () => {
+        test('Given our car park has no car when we call the escape then we will receive an empty array', () => {
+            const carParkInstance = new CarPark();
+            const carParkNoCars = [
+                [1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+            ];
+            const esacapeRoute = carParkInstance.escape(carParkNoCars);
+            expect(esacapeRoute).toStrictEqual([]);
+        });
     });
 });
--- src/carPark-escape/carpark.ts
+++ src/carPark-escape/carpark.ts
@@ -2,4 +2,32 @@ export class CarPark {
     escape(carParkNoCars: number[][]) {
         return [];
     }
+
+    /**
+     * Pseudo code:
+     *
+     * - Count the number of levels
+     * - Determine the position of your car
+     * - Determine position of staircase
+     * - Determine which level we are on
+     * - Determine how many floors we need to go down
+     *  - If it's 0, go to the exit
+     *  - Determine your position to the staircase
+     *
+     * More examples
+     * [[1, 0, 0, 0, 2],
+     * [[1, 0, 0, 0, 0],
+     * [[1, 0, 0, 0, 0],
+     * [[1, 0, 0, 0, 0],
+     * [[1, 0, 0, 0, 0],
+     * [[1, 0, 0, 0, 0],
+     * [[0, 0, 0, 0, 0]] Should return ['L4', 'D1', 'D1', 'D1', 'D1', 'D1', 'D1', 'R4']
+     *
+     * [
+     *  [0, 0, 2, 1, 0],
+     *  [0, 0, 0, 0, 0]
+     * ] Should return ['R1', 'D1', 'R1']
+     *
+     * [[0, 0, 0, 0, 0]] Should return []
+     */
 }
