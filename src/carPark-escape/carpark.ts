346a1a431abe4b88af72fe1558912cbf2f05f159
André Mathlener <andre.mathlener@ns.nl>
2024-12-12 16:00:53 +0100

    Cleanup

--- src/carPark-escape/carpark.ts
+++ src/carPark-escape/carpark.ts
@@ -5,7 +5,6 @@ export class CarPark {
         const numberOfLevels = carPark.length;
         const result: string[] = [];
 
-        //sneak preview
         let position = carPark[0].indexOf(2);
 
         carPark.forEach((level, index) => {
@@ -24,32 +23,4 @@ export class CarPark {
         });
         return result;
     }
-
-    /**
-     * Pseudo code:
-     *
-     * - Count the number of levels
-     * - Determine the position of your car
-     * - Determine position of staircase
-     * - Determine which level we are on
-     * - Determine how many floors we need to go down
-     *  - If it's 0, go to the exit
-     *  - Determine your position to the staircase
-     *
-     * More examples
-     * [[1, 0, 0, 0, 2],
-     * [[1, 0, 0, 0, 0],
-     * [[1, 0, 0, 0, 0],
-     * [[1, 0, 0, 0, 0],
-     * [[1, 0, 0, 0, 0],
-     * [[1, 0, 0, 0, 0],
-     * [[0, 0, 0, 0, 0]] Should return ['L4', 'D1', 'D1', 'D1', 'D1', 'D1', 'D1', 'R4']
-     *
-     * [
-     *  [0, 0, 2, 1, 0],
-     *  [0, 0, 0, 0, 0]
-     * ] Should return ['R1', 'D1', 'R1']
-     *
-     * [[0, 0, 0, 0, 0]] Should return []
-     */
 }
