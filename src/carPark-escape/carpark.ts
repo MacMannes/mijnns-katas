47dafe26e72766b4afefd65e4bf64c89c462ac57
André Mathlener <andre.mathlener@ns.nl>
2024-12-12 16:37:26 +0100

    Add method isBottomFloor

--- src/carPark-escape/carpark.ts
+++ src/carPark-escape/carpark.ts
@@ -5,14 +5,17 @@ export class CarPark {
         if (this.getNumberOfLevelsWithoutStairs(carPark) > 1) return ['ERR'];
 
         const floorNumberOfOurCar = carPark.findIndex((level) => level.indexOf(2) >= 0);
-        const numberOfLevels = carPark.length;
         const result: string[] = [];
 
         let positionOnFloor = carPark[floorNumberOfOurCar].indexOf(2);
 
         carPark.forEach((level, index) => {
             if (index >= floorNumberOfOurCar) {
-                if (index < numberOfLevels - 1) {
+                if (this.isBottomFloor(index, carPark)) {
+                    const numberOfSpaces = level.length;
+                    const spacesToMove = numberOfSpaces - positionOnFloor - 1;
+                    result.push(`R${spacesToMove}`);
+                } else {
                     const positionOfStaircase = level.indexOf(1);
                     const spacesToMove = positionOfStaircase - positionOnFloor;
                     const direction = positionOfStaircase > positionOnFloor ? 'R' : 'L';
@@ -21,14 +24,14 @@ export class CarPark {
                     result.push(`D1`);
 
                     positionOnFloor = positionOfStaircase;
-                } else {
-                    const numberOfSpaces = level.length;
-                    const spacesToMove = numberOfSpaces - positionOnFloor - 1;
-                    result.push(`R${spacesToMove}`);
                 }
             }
         });
         return result;
+    }
+
+    private isBottomFloor(index: number, carPark: number[][]) {
+        return index === carPark.length - 1;
     }
 
     private isOurCarInCarPark(carPark: number[][]) {
