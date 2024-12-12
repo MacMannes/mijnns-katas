5b55a8c60803f058a070eabf17a4393382bc9b9a
André Mathlener <andre.mathlener@ns.nl>
2024-12-12 15:50:56 +0100

    Refactoring: Use forEach loop

--- src/carPark-escape/carpark.ts
+++ src/carPark-escape/carpark.ts
@@ -11,23 +11,26 @@ export class CarPark {
             return [`R${spacesToMove}`];
         }
 
-        const result = [];
-        if (numberOfLevels === 2) {
-            const positionOfStaircase = carPark[0].indexOf(1);
-            const position = carPark[0].indexOf(2);
-            let spacesToMove = positionOfStaircase - position;
-            const direction = positionOfStaircase > position ? 'R' : 'L';
-            result.push(`${direction}${Math.abs(spacesToMove)}`);
-            result.push(`D1`);
-            const numberOfSpaces = carPark[0].length;
-            spacesToMove = numberOfSpaces - positionOfStaircase - 1;
-            result.push(`R${spacesToMove}`);
-        }
+        const result: string[] = [];
 
         //sneak preview
+        let position = carPark[0].indexOf(2);
 
         carPark.forEach((level, index) => {
-            // magic
+            if (index < numberOfLevels - 1) {
+                const positionOfStaircase = level.indexOf(1);
+                const spacesToMove = positionOfStaircase - position;
+                const direction = positionOfStaircase > position ? 'R' : 'L';
+                result.push(`${direction}${Math.abs(spacesToMove)}`);
+                result.push(`D1`);
+                position = positionOfStaircase;
+            }
+
+            if (index === numberOfLevels - 1) {
+                const numberOfSpaces = level.length;
+                const spacesToMove = numberOfSpaces - position - 1;
+                result.push(`R${spacesToMove}`);
+            }
         });
         return result;
     }
